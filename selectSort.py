dba9160243ce0639a25df0060ac6be4072f6c8a3
jie315 <shijie95315@outlook.com>
2021-11-17 15:53:25 +0800

    Create selectSort.py

---
+++ selectSort.py
@@ -0,0 +1,10 @@
+def selectSort(nums:list):
+    n = len(nums)
+    if n <= 1:
+        return nums
+
+    for i in range(n):
+        min_value = min(nums[i:])
+        min_index = nums.index(min_value)
+        nums[i], nums[min_index] = nums[min_index], nums[i]
+    return nums
