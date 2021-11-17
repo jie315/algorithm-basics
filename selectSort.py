efd1e05c461efbf7229fe879e4b21793c37eb03d
jie315 <shijie95315@outlook.com>
2021-11-17 15:59:59 +0800

    选择排序

--- selectSort.py
+++ selectSort.py
@@ -2,7 +2,7 @@ def selectSort(nums:list):
     n = len(nums)
     if n <= 1:
         return nums
-
+    
     for i in range(n):
         min_value = min(nums[i:])
         min_index = nums.index(min_value)
